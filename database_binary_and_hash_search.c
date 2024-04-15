b104ddf2f1f6ab60b9eadb21f36d5ae19e8f860f
Fyodor Potseluev <kommunist90@gmail.com>
2024-04-15 11:35:45 +0300

    - an existing entry can be found in logarithmic time O(log N);     -- the Hoare quick sort algorithm is used after adding a new entry to provide working conditions for binary search;     -- the binary search algorithm is used every time we need to find an existing entry; - size of element is now 64 bytes instead of 68; - "60 bytes long string could be written" bug fixed; - `input_output_test.sh` and `memory_leak_test.sh` updated.

--- database_binary_and_hash_search.c
+++ database_binary_and_hash_search.c
@@ -1,6 +1,7 @@
 /* database_binary_and_hash_search.c */
 
 #include <stdlib.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
@@ -15,7 +16,7 @@ enum constants{
 };
 
 typedef struct tag_entry {
-    char search_key[max_entry_name_size + 1];
+    char str[max_entry_name_size];
     int data;
 } entry;
 
@@ -33,7 +34,7 @@ void entry_name_error_handling(const char *entry_name)
         printf("Error: Item name has not been specified\n");
         exit(1);
     }
-    if (strlen(entry_name) > max_entry_name_size) {
+    if (strlen(entry_name) >= max_entry_name_size) {
         printf("Error: Too long entry name\n");
         exit(1);
     }
@@ -58,6 +59,142 @@ void file_close_error_handling(int fclose_res, const char *file_name)
     }
 }
 
+int size_of_file(FILE *file)
+{
+    /* this function sets the file position at the end of file! */
+    fseek(file, 0, SEEK_END);
+    return(ftell(file) / sizeof(entry));
+}
+
+void get_entry(entry **source, FILE *file, int file_pos)
+{
+    fseek(file, file_pos*sizeof(entry), SEEK_SET);
+    fread(*source, sizeof(entry), 1, file);
+}
+
+void swap_left_and_right(
+    FILE *file, const entry *left, const entry *right,
+    int left_pos, int right_pos
+)
+{
+    entry *tmp = malloc(sizeof(entry));
+
+    /* tmp = left; */
+    memcpy(tmp, left, sizeof(entry));
+    /* left = right; */
+    fseek(file, left_pos*sizeof(entry), SEEK_SET);
+    fwrite(right, sizeof(entry), 1, file);
+    /* right = tmp; */
+    fseek(file, right_pos*sizeof(entry), SEEK_SET);
+    fwrite(tmp, sizeof(entry), 1, file);
+
+    free(tmp);
+}
+
+/* Variadic function. */
+/* The last parameter in the parameter list must be equal to NULL - this */
+/* allows the function to understand that the parameter list has ended.*/
+void clean_up(entry *first, ...)
+{
+    va_list vl;
+    entry *p;
+    va_start(vl, first);
+    for (p=first; p; p=va_arg(vl, entry *))
+        free(p);
+    va_end(vl);
+}
+
+int part_of_quick_sort_hoare(FILE *file, int left_pos, int right_pos)
+{
+    entry *left = malloc(sizeof(entry));
+    entry *pivot = malloc(sizeof(entry));
+    entry *right = malloc(sizeof(entry));
+    get_entry(&pivot, file, left_pos+(right_pos-left_pos)/2);
+    while (left_pos <= right_pos) {
+        /* while (left->str < pivot->str) left_pos++ */
+        get_entry(&left, file, left_pos);
+        while (strcmp(left->str, pivot->str) < 0) {
+            left_pos++;
+            get_entry(&left, file, left_pos);
+        }
+        /* while (pivot->str < right->str) right_pos-- */
+        get_entry(&right, file, right_pos);
+        while (strcmp(pivot->str, right->str) < 0) {
+            right_pos--;
+            get_entry(&right, file, right_pos);
+        }
+        if (left_pos <= right_pos) {
+            swap_left_and_right(file, left, right, left_pos, right_pos);
+            left_pos++;
+            right_pos--;
+        }
+    }
+    clean_up(left, pivot, right, NULL);
+    return left_pos;
+}
+
+void quick_sort_hoare_reqursive_call(FILE *file, int start, int end)
+{
+    if (start >= end) return;
+    int right_start_position = part_of_quick_sort_hoare(file, start, end);
+    quick_sort_hoare_reqursive_call(file, start, right_start_position-1);
+    quick_sort_hoare_reqursive_call(file, right_start_position, end);
+}
+
+void quick_sort_hoare(FILE *file)
+{
+    int file_size = size_of_file(file);
+    quick_sort_hoare_reqursive_call(file, 0, file_size-1);
+}
+
+int find_entry_with_binary_search(
+    FILE *file, const char *file_name, const char *entry_name
+)
+{
+    int file_size = size_of_file(file);
+    int left = 0, mid, right = file_size-1;
+    entry *mid_entry = malloc(sizeof(entry));
+    for(;;) {
+        int read_res;
+        if (left > right){
+            /* there is no such entry */
+            free(mid_entry);
+            return -1;
+        }
+        mid = left + (right - left) / 2;
+        fseek(file, mid*sizeof(entry), SEEK_SET);
+        read_res = fread(mid_entry, sizeof(entry), 1, file);
+        if (!read_res)
+            read_error_handling(file, file_name);
+        /* entry_name < mid_entry->str */
+        if (strcmp(entry_name, mid_entry->str) < 0) right = mid-1;
+        /* mid_entry->str < entry_name */
+        if (strcmp(mid_entry->str, entry_name) < 0) left = mid+1;
+        /* mid_entry->str == entry_name */
+        if (strcmp(mid_entry->str, entry_name) == 0) {
+            /* entry found */
+            free(mid_entry);
+            return mid;
+        }
+    }
+}
+
+void make_new_entry(FILE *file, const char *entry_name, entry *buffer)
+{
+    strcpy(buffer->str, entry_name);
+    buffer->data = 1;
+    fwrite(buffer, 1, sizeof(entry), file);
+}
+
+void edit_entry(FILE *file, int file_pos, entry *buffer)
+{
+    fseek(file, file_pos*sizeof(entry), SEEK_SET);
+    fread(buffer, sizeof(entry), 1, file);
+    buffer->data += 1;
+    fseek(file, file_pos*sizeof(entry), SEEK_SET);
+    fwrite(buffer, sizeof(entry), 1, file);
+}
+
 void add_entry(char *const *argv)
 {
     const char *file_name = argv[file_position];
@@ -67,30 +204,23 @@ void add_entry(char *const *argv)
     int file_pos = 0;
     entry_name_error_handling(entry_name);
     if (file) {
-        /* find existing entry then edit it */
-        while (fread(buffer, 1, sizeof(entry), file)) {
-            if (0 == strcmp(buffer->search_key, entry_name)) {
-                buffer->data += 1;
-                fseek(file, file_pos*sizeof(entry), SEEK_SET);
-                fwrite(buffer, 1, sizeof(entry), file);
-                /* if entry was found and edited - close the file */
-                goto file_close;
-            }
-            file_pos++;
+        file_pos = find_entry_with_binary_search(file, file_name, entry_name);
+        if (file_pos != -1) {
+            /* entry was found */
+            edit_entry(file, file_pos, buffer);
+            goto file_close;
         }
-        read_error_handling(file, file_name);
-        fseek(file, file_pos*sizeof(entry), SEEK_SET);
+        /* entry was not found */
+        fseek(file, 0, SEEK_END);
     } else {
-        /* if there was an error trying to open existing file */
+        /* if there was an error trying to open existing file: */
         if (0 == access(file_name, F_OK))
             file_open_error_handling(file, file_name);
         file = fopen(file_name, "w");
         file_open_error_handling(file, file_name);
     }
-    /* make a new entry */
-    strcpy(buffer->search_key, entry_name);
-    buffer->data = 1;
-    fwrite(buffer, 1, sizeof(entry), file);
+    make_new_entry(file, entry_name, buffer);
+    quick_sort_hoare(file);
     file_close:
     free(buffer);
     int fclose_res = fclose(file);
@@ -105,15 +235,13 @@ void print_entry(char *const *argv)
     FILE *file = fopen(file_name, "r");
     file_open_error_handling(file, file_name);
     entry_name_error_handling(entry_name);
-    while (fread(read_res, 1, sizeof(entry), file)) {
-        if (0 == strcmp(entry_name, read_res->search_key)) {
-            printf("%s - %d\n", entry_name, read_res->data);
-            goto file_close;
-        }
-    }
-    read_error_handling(file, file_name);
-    printf("%s - 0\n", entry_name);
-    file_close:
+    int file_pos = find_entry_with_binary_search(file, file_name, entry_name);
+    if (file_pos != -1) {
+        fseek(file, file_pos*sizeof(entry), SEEK_SET);
+        fread(read_res, sizeof(entry), 1, file);
+        printf("%s - %d\n", entry_name, read_res->data);
+    } else
+        printf("%s - 0\n", entry_name);
     free(read_res);
     int fclose_res = fclose(file);
     file_close_error_handling(fclose_res, file_name);
@@ -126,7 +254,7 @@ void print_all_entries(char *const *argv)
     FILE *file = fopen(file_name, "r");
     file_open_error_handling(file, file_name);
     while (fread(read_res, 1, sizeof(entry), file)) {
-        printf("%s - %d\n", read_res->search_key, read_res->data);
+        printf("%s - %d\n", read_res->str, read_res->data);
     }
     free(read_res);
     int fclose_res = fclose(file);
--- input_output_test.sh
+++ input_output_test.sh
@@ -6,7 +6,7 @@ expected=()
 input+=("test.bin add apple pear")
 expected+=( "Error: Incorrect number of arguments" )
 
-input+=("test.bin add this_is_too_long_string_that_surpasses_the_60_characters_limit_and_causes_Too_Long_ID_Error")
+input+=("test.bin add string_which_consist_of_60_chars_which_surpas_the_limit_by_1")
 expected+=( "Error: Too long entry name" )
 
 input+=("test.bin query apple pear")
@@ -21,38 +21,145 @@ expected+=( "Error: Incorrect number of arguments" )
 input+=("test.bin badd apple")
 expected+=( "Error: Incorrect command" )
 
-input+=("test.bin add apple")
+input+=("test.bin add pear")
+expected+=( "" )
+
+input+=("test.bin add carrot")
+expected+=( "" )
+
+input+=("test.bin add pumpkin")
 expected+=( "" )
 
 input+=("test.bin add pear")
+expected+=( "" )
+
+input+=("test.bin query pear")
+expected+=( "pear - 2" )
+
+input+=("test.bin query carrot")
+expected+=( "carrot - 1" )
+
+input+=("test.bin query pumpkin")
+expected+=( "pumpkin - 1" )
+
+input+=("test.bin list")
+expected+=( $'carrot - 1\npear - 2\npumpkin - 1' )
+
+input+=("test.bin add pear")
+expected+=( "" )
+
+input+=("test.bin add cucumber")
+expected+=( "" )
+
+input+=("test.bin add cabbage")
+expected+=( "" )
+
+input+=("test.bin add cucumber")
+expected+=( "" )
+
+input+=("test.bin add pineapple")
+expected+=( "" )
+
+input+=("test.bin add pineapple")
+expected+=( "" )
+
+input+=("test.bin add pumpkin")
 expected+=( "" )
 
 input+=("test.bin add tomato")
 expected+=( "" )
 
-input+=("test.bin add pear")
+input+=("test.bin query cucumber")
+expected+=( "cucumber - 2" )
+
+input+=("test.bin add banana")
 expected+=( "" )
 
-input+=("test.bin query this_is_too_long_string_that_surpasses_the_60_characters_limit_and_causes_Too_Long_ID_Error")
-expected+=( "Error: Too long entry name" )
+input+=("test.bin query pumpkin")
+expected+=( "pumpkin - 2" )
 
-input+=("test.bin query apple")
-expected+=( "apple - 1" )
+input+=("test.bin query cabbage")
+expected+=( "cabbage - 1" )
 
 input+=("test.bin query pear")
-expected+=( "pear - 2" )
+expected+=( "pear - 3" )
+
+input+=("test.bin query carrot")
+expected+=( "carrot - 1" )
 
 input+=("test.bin query tomato")
 expected+=( "tomato - 1" )
 
-input+=("test.bin query pineapple")
-expected+=( "pineapple - 0" )
-
-input+=("test.bin list this_is_too_long_string_that_surpasses_the_60_characters_limit_and_causes_Too_Long_ID_Error")
-expected+=( $'apple - 1\npear - 2\ntomato - 1' )
+# Query non existing item
+input+=("test.bin query mango")
+expected+=( "mango - 0" )
 
 input+=("test.bin list")
-expected+=( $'apple - 1\npear - 2\ntomato - 1' )
+expected+=( $'banana - 1\ncabbage - 1\ncarrot - 1\ncucumber - 2\npear - 3\npineapple - 2\npumpkin - 2\ntomato - 1' )
+
+input+=("test.bin add onion")
+expected+=( "" )
+
+input+=("test.bin add pear")
+expected+=( "" )
+
+input+=("test.bin query pineapple")
+expected+=( "pineapple - 2" )
+
+input+=("test.bin add onion")
+expected+=( "" )
+
+input+=("test.bin add pineapple")
+expected+=( "" )
+
+input+=("test.bin add potato")
+expected+=( "" )
+
+# add maximum size entry name
+input+=("test.bin add an_entry_which_consist_of_exactly_59_characters_long_string")
+expected+=( "" )
+
+input+=("test.bin query pineapple")
+expected+=( "pineapple - 3" )
+
+input+=("test.bin query an_entry_which_consist_of_exactly_59_characters_long_string")
+expected+=( "an_entry_which_consist_of_exactly_59_characters_long_string - 1" );
+
+input+=("test.bin query onion")
+expected+=( "onion - 2" )
+
+input+=("test.bin query pear")
+expected+=( "pear - 4" )
+
+input+=("test.bin query cucumber")
+expected+=( "cucumber - 2" )
+
+input+=("test.bin query cabbage")
+expected+=( "cabbage - 1" )
+
+input+=("test.bin query pumpkin")
+expected+=( "pumpkin - 2" )
+
+input+=("test.bin query potato")
+expected+=( "potato - 1" )
+
+input+=("test.bin query banana")
+expected+=( "banana - 1" )
+
+input+=("test.bin query tomato")
+expected+=( "tomato - 1" )
+
+input+=("test.bin query carrot")
+expected+=( "carrot - 1" )
+
+input+=("test.bin list")
+expected+=( $'an_entry_which_consist_of_exactly_59_characters_long_string - 1\nbanana - 1\ncabbage - 1\ncarrot - 1\ncucumber - 2\nonion - 2\npear - 4\npineapple - 3\npotato - 1\npumpkin - 2\ntomato - 1' )
+
+input+=("test.bin query string_which_consist_of_60_chars_which_surpas_the_limit_by_1")
+expected+=( "Error: Too long entry name" )
+
+input+=("test.bin list string_which_consist_of_60_chars_which_surpas_the_limit_by_1")
+expected+=( $'an_entry_which_consist_of_exactly_59_characters_long_string - 1\nbanana - 1\ncabbage - 1\ncarrot - 1\ncucumber - 2\nonion - 2\npear - 4\npineapple - 3\npotato - 1\npumpkin - 2\ntomato - 1' )
 
 input+=("test.bin query")
 expected+=( "Error: Item name has not been specified" )
@@ -61,7 +168,7 @@ i=0
 arr_len=${#input[@]}
 printf '\n'
 for idx in "${!input[@]}"; do
-    actual=$(../database_binary_and_hash_search ${input[idx]})
+    actual=$(./database_binary_and_hash_search ${input[idx]})
     if [[ "${expected[idx]}" != "$actual" ]]; then
         printf '%s\n\n' \
             '---------------------------------------------------------------'
